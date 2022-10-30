792c32a671311fa6303d27ff55cecf4d5c14ceeb
febryanmz <febryan.zainal@gmail.com>
2022-10-30 21:10:43 +0700

    first commit

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -4,6 +4,26 @@ import "fmt"
 
 func DrawXYZ(n int) string {
 	// your code here
+	var pangkat int = 2
+	var bobot string
+	var test int = 1
+	for i := 0; i < pangkat; i++ {
+		test *= n
+	}
+	for i := 1; i <= test; i++ {
+		if i%3 == 0 {
+			bobot += "X "
+		} else if i%2 == 0 {
+			bobot += "Z "
+		} else {
+			bobot += "Y "
+		}
+		if i%n == 0 {
+			bobot += "\n"
+		}
+	}
+	return bobot
+
 }
 
 func main() {
